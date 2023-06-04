3aa563a03bec918a16b3ecde043b2c669470e04c
devsourcenathan <sadenachbinathan@gmail.com>
2023-06-04 07:12:39 +0100

    add navigation

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:agro_tech_app/pages/cart_page.dart';
+import 'package:agro_tech_app/pages/shop_page.dart';
+import 'package:agro_tech_app/widgets/bottom_nav_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  //page
+  final List<Widget> _pages = [
+    const ShopPage(),
+    const CartPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyBottomNavigationBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
+    );
+  }
+}
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,87 @@
+import 'package:agro_tech_app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+
+              Padding(
+                padding: EdgeInsets.all(25.0),
+                child: Image.asset(
+                  "lib/images/avocat.jpg",
+                  height: 240,
+                ),
+              ),
+
+              const SizedBox(
+                height: 48,
+              ),
+              //title
+
+              const Text(
+                'Just Do It',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+
+              const SizedBox(
+                height: 24,
+              ),
+              //sub title
+              const Text(
+                'Je ne connais pas encore ce que je vais ecrire ici, mais il faut quand meme que ce soit sur 2 lignes',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(
+                height: 40,
+              ),
+              //start button
+              GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: const EdgeInsets.all(25),
+                  child: Center(
+                    child: Text(
+                      'Shop now',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
